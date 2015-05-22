d13abf927538067715475051b5a8f1b8f622244c
mamaker <madhuvasudevan@hotmail.com>
2015-05-22 00:41:15 -0500

    Done!

--- DictionaryInAction/Form1.cs
+++ DictionaryInAction/Form1.cs
@@ -16,5 +16,20 @@ namespace DictionaryInAction
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, string> wordDefinition =
+            new Dictionary<string, string>();
+            wordDefinition.Add("Dictionary", "A book that lists the words of a "
+            + "language in alphabetical order and gives their meaning");
+            wordDefinition.Add("Key", "A thing that provides a means of gaining access to "
+            + "our understanding something.");
+            wordDefinition.Add("Value", "A quantity, number, string, or reference.");
+            if (wordDefinition.ContainsKey("Key"))
+            {
+                MessageBox.Show(wordDefinition["Key"]);
+            }
+        }
     }
 }
